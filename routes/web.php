f65b6b688bc80bdb03cf64e6bfc232ed737c163b
Idehen Divine <idehendivine16@gmail.com>
2025-08-18 06:04:05 +0100

    feat: define routes for Livewire components in wedding application

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Livewire\DetailsPage;
+use App\Livewire\GalleryPage;
+use App\Livewire\HomePage;
+use App\Livewire\RsvpPage;
+use App\Livewire\StoryPage;
+use App\Livewire\WishesPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomePage::class)->name('home');
+Route::get('/details', DetailsPage::class)->name('details');
+Route::get('/rsvp', RsvpPage::class)->name('rsvp');
+Route::get('/gallery', GalleryPage::class)->name('gallery');
+Route::get('/wishes', WishesPage::class)->name('wishes');
+Route::get('/story', StoryPage::class)->name('story');
