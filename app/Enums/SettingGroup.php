ea3179dec19d2567bc7aaf0dca9c5d0524f7f8c2
Idehen Divine <idehendivine16@gmail.com>
2025-08-24 14:17:25 +0100

    feat: add SettingGroup and SettingType enums for configuration management

---
+++ app/Enums/SettingGroup.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum SettingGroup: string
+{
+    case GENERAL = 'general';
+    case CEREMONY = 'ceremony';
+    case RECEPTION = 'reception';
+    case SOCIAL = 'social';
+    case CONTACT = 'contact';
+}
---
+++ app/Enums/SettingType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum SettingType: string
+{
+    case TEXT = 'text';
+    case TEXTAREA = 'textarea';
+    case DATE = 'date';
+    case TIME = 'time';
+    case URL = 'url';
+    case BOOLEAN = 'boolean';
+}
