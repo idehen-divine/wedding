fef950affad774d7a4a0d7c75c24a96f7d922837
Idehen Divine <idehendivine16@gmail.com>
2025-08-27 05:50:45 +0100

    feat: implement gallery published state in GalleryPage component

--- app/Livewire/GalleryPage.php
+++ app/Livewire/GalleryPage.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\WeddingSetting;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 #[Layout('components.layouts.wedding')]
 class GalleryPage extends Component
 {
+    public bool $isPublished = false;
+
+    public function mount()
+    {
+        $this->isPublished = WeddingSetting::get('gallery_published', false);
+    }
+
     public function render()
     {
         return view('livewire.gallery-page')->title('Photo Gallery - Precious & Franklin Wedding');
